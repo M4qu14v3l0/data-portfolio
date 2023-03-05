866881755edf55a6fdae3877a22d379662d82807
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2023-03-04 21:20:12 -0500

    skills added

--- src/components/sideBar/Skills.jsx
+++ src/components/sideBar/Skills.jsx
@@ -1,8 +1,23 @@
+
+const listSkills = [
+  {name:'Python'},
+  {name:'SQL'},
+  {name:'Machine Learning'},
+  {name:'Deep Learning'}
+]
+
+
 export default function Skills() {
   return (
-    <div>
-
-
-    </div>
+    <section className="flex flex-col gap-3">
+        <h3 className='text-xs'>Skills</h3>
+        <div className="flex gap-3">
+          {
+            listSkills.map(skill => (
+              <p key={skill.name} className="text-sm text-center text-blue-600 bg-blue-100 p-1 rounded-full">{skill.name}</p>
+            ))
+          }
+        </div>
+    </section>
   )
 }
